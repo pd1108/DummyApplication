92c9a7475f0a18ea0cc4e6d75d23f7c179220cf3
PRASHANT <prashant.dash7600@gmail.com>
2022-11-20 12:06:49 +0530

    fifth commit containing error

--- app/src/main/java/com/example/dummyapplication/MainActivity.java
+++ app/src/main/java/com/example/dummyapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("For second commit!!!");
-        System.out.println("Error code!!!");
+        System.out.println("Error code");
     }
 }
